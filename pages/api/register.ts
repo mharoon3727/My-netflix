ebe16ec63b605c1c3a02179cb86a8274ce8451df
Muhammad Haroon Rasheed <haroonrasheed211f@gmail.com>
2023-11-18 19:54:13 +0500

    fix typos

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -17,7 +17,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     })
 
     if (existingUser) {
-      return res.status(422).json({ error: 'Email taken' });
+      return res.status(422).json({ error: 'Email already taken' });
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
